93a5ab123e856c35b491ca0bb06a43e5d574d7d8
SpiderHimel <minhuztaznim@gmail.com>
2022-09-28 11:53:15 +0600

    Multi line comment

---
+++ mlc.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main()
+{
+    /* This is an example of multi line comment. Any text between this will be ignored by the compiler*/
+    printf("My name is Himel.\n");
+    return 0;
+}
