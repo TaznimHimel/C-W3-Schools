c443db983e35e436dda3a2d17c3fc78aa76158bc
SpiderHimel <minhuztaznim@gmail.com>
2022-09-28 14:31:19 +0600

    Details Added

--- constant.c
+++ constant.c
@@ -11,3 +11,5 @@ int main() {
 
 
 //You should always declare the variable as constant when you have values that are unlikely to change.
+//When you declare a constant variable, it must be assigned with a value like this const int minutesPerHour = 60;
+//Another thing about constant variables, is that it is considered good practice to declare them with uppercase. It is not required, but useful for code readability and common for C programmers.Like this const int BIRTHYEAR = 1980;
