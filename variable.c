d469c16f5419927903dd07f478c5f8250aad9451
SpiderHimel <minhuztaznim@gmail.com>
2022-09-28 14:23:58 +0600

    Details added

--- variable.c
+++ variable.c
@@ -12,3 +12,11 @@ int main() {
   printf("%c\n", myLetter);
   return 0;
 }
+
+/*The general rules for naming variables are:
+
+Names can contain letters, digits and underscores
+Names must begin with a letter or an underscore (_)
+Names are case sensitive (myVar and myvar are different variables)
+Names cannot contain whitespaces or special characters like !, #, %, etc.
+Reserved words (such as int) cannot be used as names */
