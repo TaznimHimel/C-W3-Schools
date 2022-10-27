dfece4a2868775ac8d4f7c5a05603bd9c9cd9c1a
Himel <minhuztaznim@gmail.com>
2022-10-27 15:46:19 +0600

    Function

---
+++ function.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+// Function declaration
+void myFunction();
+
+// The main method
+int main() {
+  myFunction();  // call the function
+  return 0;
+}
+
+// Function definition
+void myFunction() {
+  printf("I am Minhuz Taznim Himel");
+}
+
+//void myFunction() {// declaration
+  // the body of the function (definition)}
